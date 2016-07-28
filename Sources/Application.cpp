282f7fea5c2303ca715b8f052748edb13e906229
DomDom on macbook <dom.dumont@gmail.com>
2016-07-28 12:12:09 +0200

    OpenGL mini test

--- Sources/Application.cpp
+++ Sources/Application.cpp
@@ -9,21 +9,15 @@ bool Application::Init(std::string title, int xpos, int ypos, int width,int heig
 {
 	std::string tempTitle;
 	tempTitle=  title + " " + GIT_DESCRIBE_VERSION;
-	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 		{
-
-		// init the window
-		pWindow = SDL_CreateWindow(tempTitle.c_str(), xpos, ypos, width, height, flags);
-		if (pWindow != 0) // window init success
-			{
-			pRenderer = SDL_CreateRenderer(pWindow, -1, 0);
-			}
-		else
-			{
-				return false;
-			}
+		return false;
 		}
-
+		
+	if (SDL_CreateWindowAndRenderer(width, height, SDL_WINDOW_OPENGL, &pWindow, &pRenderer) < 0)
+	{
+		return false;
+	}
 	AudioManager::Get().Init();
 
 	bRunning = true;
@@ -71,15 +65,30 @@ void Application::Update()
 
 void Application::Render()
 {
-	SDL_RenderClear(pRenderer); 
+ 	/* Set the background black */
+    glClearColor( 1.0f, 0.0f, 0.0f, 0.0f );
+    /* Clear The Screen And The Depth Buffer */
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
-	Font *pTempFont = new Font();
-	pTempFont->Load("Data/pico8.ttf", 16);
-	Color tempColor;
-	tempColor.r = 255;
-	SDL_Texture * tempTexture = pTempFont->RenderText("Coucou", tempColor);
-	
-	SDL_RenderCopy(g_app->pRenderer, tempTexture, NULL, NULL);
+    /* Move Left 1.5 Units And Into The Screen 6.0 */
+    glLoadIdentity();
+    glTranslatef( -1.5f, 0.0f, -6.0f );
 
-	SDL_RenderPresent(pRenderer); 
+    glBegin( GL_TRIANGLES );            /* Drawing Using Triangles */
+      glVertex3f(  0.0f,  1.0f, 0.0f ); /* Top */
+      glVertex3f( -1.0f, -1.0f, 0.0f ); /* Bottom Left */
+      glVertex3f(  1.0f, -1.0f, 0.0f ); /* Bottom Right */
+    glEnd( );                           /* Finished Drawing The Triangle */
+
+    /* Move Right 3 Units */
+    glTranslatef( 3.0f, 0.0f, 0.0f );
+
+    glBegin( GL_QUADS );                /* Draw A Quad */
+      glVertex3f( -1.0f,  1.0f, 0.0f ); /* Top Left */
+      glVertex3f(  1.0f,  1.0f, 0.0f ); /* Top Right */
+      glVertex3f(  1.0f, -1.0f, 0.0f ); /* Bottom Right */
+      glVertex3f( -1.0f, -1.0f, 0.0f ); /* Bottom Left */
+    glEnd( );                           /* Done Drawing The Quad */
+    
+	SDL_RenderPresent(pRenderer);
 }
